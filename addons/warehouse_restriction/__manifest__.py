22a0b3f5d746f0556d9a11c47937d447b611f177
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2025-01-15 19:40:34 +0000

    turned 'application' to true

--- addons/warehouse_restriction/__manifest__.py
+++ addons/warehouse_restriction/__manifest__.py
@@ -5,13 +5,13 @@
     'category': 'Warehouse',
     'summary': 'Restricción de acceso a almacenes por usuario.',
     'description': 'Permite definir los almacenes permitidos a los usuarios de manera similar a la configuración multiempresa.',
-    'depends': ['stock'],
+    'depends': ['base', 'stock'],
     'data': [
         'security/warehouse_restriction_security.xml',
         'security/ir.model.access.csv',
         'views/res_users_view.xml',
     ],
     'installable': True,
-    'application': False,
+    'application': True,
     'auto_install': False,
 }
